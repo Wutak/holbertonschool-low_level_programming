4732ce7c8a529243612f15873f99a46dcfdd7344
julien <julienvillessot@gmail.com>
2024-10-22 13:31:30 +0000

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "main.h"
 /**
  * main - check the code
  *
@@ -7,9 +8,4 @@
 void  print_alphabet(void)
 {
 	write(1, "abcdefghijklmnoparstuvwxyz\n", 27);
-}
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
