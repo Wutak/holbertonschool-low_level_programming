222105b4848b846bb131df5f1381ffa98882dd82
julien <julienvillessot@gmail.com>
2024-10-23 12:07:22 +0000

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+/**
+ * _isalpha - Entry point.
+ * @c: input.
+ * Description: alphabetic.
+ * Return: 1 (Succes), 0.
+ */
+int _isalpha(int c)
+{
+	if (c <= 90 && c >= 65 || c <= 122 && c >= 97)
+		return (1);
+	else
+		return (0);
+}
