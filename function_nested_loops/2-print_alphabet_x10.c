a51356957f8f7e8dea15cad8016c5d0ed71139e3
julien <julienvillessot@gmail.com>
2024-10-23 08:23:59 +0000

    x10

---
+++ function_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "2-main.c"
+#include <unistd.h>
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: alphabet x10
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	int v;
+	int i;
+
+	v = 'a';
+	i = 0;
+	while (i < 10)
+	{
+		while (v <= 'z')
+		{
+			write(1, &v, 1);
+			v++;
+		}
+		i++;
+	}
+}
