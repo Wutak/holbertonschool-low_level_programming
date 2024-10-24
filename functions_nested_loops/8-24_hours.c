0441acb53db9330fb57bce39e7fdc487cdbb0135
julien <julienvillessot@gmail.com>
2024-10-24 12:04:14 +0000

    clock

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+/**
+ * jack_bauer - Entry.
+ * Description: clock.
+ * Return: Nothing.
+ */
+void jack_bauer(void)
+{
+	int m;
+	int h;
+
+	m = 0;
+	h = 0;
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+			write(1, "(h / 10) + '0'", 2);
+			write(1, "(h % 10) + '0'", 2);
+			write(1, ":", 1);
+			write(1, "(m / 10) + '0'", 2);
+			write(1, "(m % 10) + '0'", 2);
+			write(1, "\n", 1);
+			m++;
+		}
+		h++;
+		m = 0;
+	}
+}
