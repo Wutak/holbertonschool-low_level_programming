94dff4f1b7932aa86e18ef364e1a8717c2514752
julien <julienvillessot@gmail.com>
2024-10-24 13:15:32 +0000

    clock

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -15,14 +15,14 @@ void jack_bauer(void)
 	{
 		while (m < 60)
 		{
-			h = h / 10 + '0';
+			h = (h / 10) + '0';
 			write(1, "h", 1);
-			h = h % 10 + '0';
+			h = (h % 10) + '0';
 			write(1, "h", 1);
 			write(1, ":", 1);
-			m = m / 10 + '0';
+			m = (m / 10) + '0';
 			write(1, "m", 1);
-			m = m % 10 + '0';
+			m = (m % 10) + '0';
 			write(1, "m", 1);
 			write(1, "\n", 1);
 			m++;
