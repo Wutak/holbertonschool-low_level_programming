ad4da2657dbf82db97ae6fca518a86c889f5bee8
julien <julienvillessot@gmail.com>
2024-10-24 09:50:29 +0000

    fix

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * print_last_digit - Entry point
+ * @r: input.
+ * Description: last digit.
+ * return: Return modelo.
+ */
+int print_last_digit(int r)
+{
+	int n;
+
+	if (r < 0)
+		n = -1 * (r % 10);
+	else
+		n = r % 10;
+	return (n % 10);
+}
