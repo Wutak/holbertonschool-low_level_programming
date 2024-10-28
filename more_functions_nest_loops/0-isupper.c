9ead5bf5fd443bf66014c7cd913eecfd15783269
julien <julienvillessot@gmail.com>
2024-10-28 08:31:57 +0000

    isupper

---
+++ more_functions_nest_loops/0-isupper.c
@@ -0,0 +1,14 @@
+/**
+ * _isupper - Entry
+ * @c: input
+ * Description: check upper
+ * Return: 1, 2.
+ */
+int _isupper(int c)
+{
+	if (c <= 90 || c >= 65)
+		return (1);
+	else
+		return (0);
+}
+
