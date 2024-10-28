87099ef27c83740932f6966584643c998df07e08
julien <julienvillessot@gmail.com>
2024-10-28 10:13:11 +0000

    diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+/**
+ * print_diagonal - Entry
+ * @n: input.
+ * Description: diagonal
+ * Return: Nothing.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int v;
+
+	i = 0;
+	if (n <= 0)
+		write(1, "", 0);
+	else if (n == 1)
+		write(1, "\n", 1);
+		write(1, "\\", 1);
+	while (i < n)
+	{
+		v = 0;
+		while (v < i - 1)
+		{
+			write(1, " ", 1);
+			v++;
+		}
+		write(1, "\\", 1);
+		i++;
+	}
+}
