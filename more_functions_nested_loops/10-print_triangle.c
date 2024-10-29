eb47cb1973c5cdfbe4bc7f927bb670e2543e80d8
julien <julienvillessot@gmail.com>
2024-10-29 09:09:00 +0000

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -19,23 +19,23 @@ void print_triangle(int size)
 	s = size;
 	if (size <= 0)
 		write(1, "\n", 1);
-	else if(size == 2)
+	else if (size == 2)
 	{
 		write(1, " #\n", 3);
 		write(1, "##\n", 3);
 	}
 	else
 	{
-		while(i < size)
+		while (i < size)
 		{
 			v = 0;
-			d = 0;
-			while(v < s - 1)
+			d = 0
+			while (v < s - 1)
 			{
 				write(1, " ", 1);
 				v++;
 			}
-			while(v + d < size)
+			while (v + d < size)
 			{
 				write(1, "#", 1);
 				d++;
