1c40d852e1964b1cc8091259b798df7aac5edeea
julien <julienvillessot@gmail.com>
2024-10-29 10:33:44 +0000

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+/**
+ * _puts - Entry
+ * @str: input
+ * Description: str
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str, 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
