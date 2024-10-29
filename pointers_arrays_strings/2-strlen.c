9bf2b891a34c8cc09259a1d16660812073c0baaf
julien <julienvillessot@gmail.com>
2024-10-29 10:17:07 +0000

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen - Entry
+ * @s: input
+ * Description: strlen
+ * Return: 1.
+ */
+int _strlen(char *s)
+{
+	int i;
+	int c;
+
+	i = 0;
+	c = 0;
+	while(&c != '\0')
+	{
+		i++;
+	}
+	return (1);
+}
