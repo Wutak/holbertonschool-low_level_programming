4a53345538472058c92ca2f8e00e62d915e625e2
julien <julienvillessot@gmail.com>
2024-10-29 11:08:51 +0000

    puts

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,24 @@
+#include <unistd.h>
+/**
+ * print_rev - Entry
+ * @s: input
+ * Description: reverse
+ * Return: Nothing
+ */
 void print_rev(char *s)
 {
 	int i;
 
 	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i > 0 && i != '\0')
+	{
+		write(1, &s[i], 1);
+		i--;
+	}
+	write(1, "\n", 1);
+}
