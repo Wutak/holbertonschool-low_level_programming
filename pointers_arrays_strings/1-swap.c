a4b0722ddc8becf8fa984147cea133974e6a70b4
julien <julienvillessot@gmail.com>
2024-10-29 09:42:07 +0000

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - Entry
+ * @a: input
+ * @b: input
+ * Description: temporary
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b);
+{
+	int tmp;
+
+	tmp = a;
+	*a = b;
+	*b = tmp;
+}
