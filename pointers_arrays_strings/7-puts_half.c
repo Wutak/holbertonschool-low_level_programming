dcad1322e070fdcc9750e056f509cdf25c2f57b4
julien <julienvillessot@gmail.com>
2024-10-30 14:33:12 +0000

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+/**
+ * puts_half
+ * @str: input
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	m = i / 2;
+	if (i % 2 == 1)
+		m++;
+	while (m < i)
+	{
+		write(1, &str[m], 1);
+		m++;
+	}
+	write(1, '\n', 1);
+}
