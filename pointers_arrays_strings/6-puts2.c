bc797c388540f5bed528b7b3306caa626dc6c36b
julien <julienvillessot@gmail.com>
2024-10-30 09:46:43 +0000

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+/**
+ * puts2 - Entry
+ * @str: input
+ * Description: puts
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int v;
+
+	v = 0;
+	while (str[v] != '\0')
+	{
+		if (v % 2 == 0)
+		{
+			write(1, &str[v], 1);
+			v++;
+		}
+			v++;
+	}
+}
