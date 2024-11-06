9007d3bf38830c73914a3c4b4c5bd60c6b3af2fa
julien <julienvillessot@gmail.com>
2024-11-06 10:13:54 +0000

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -17,25 +17,3 @@ char *_strchr(char *s, char c)
 	else
 		return (0);
 }
-
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s = "hello";
-    char *f;
-
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
-}
