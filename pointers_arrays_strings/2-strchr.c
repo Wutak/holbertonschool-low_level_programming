6b4ee251f6faa10c801588ca97dec58052da320e
julien <julienvillessot@gmail.com>
2024-11-06 09:26:21 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+ * _strchr - Entry
+ * @s: input
+ * @c: input
+ * Description: strchr
+ * Return: 1, 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != c && s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s);
+		else
+			i++;
+	}
+	return (NULL);
+}
