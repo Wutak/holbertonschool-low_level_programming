42ad56f52717fec9cffe4c6b74e3eb5e1943f6bf
julien <julienvillessot@gmail.com>
2024-11-06 09:30:49 +0000

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,12 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i] != c && s[i] != '\0')
+	while ((s[i] != c) && (s[i] != '\0'))
 	{
-		if (s[i] == c)
-			return (s);
-		else
+		if (s[i] != c)
 			i++;
+		else
+			return (s);
 	}
 	return (NULL);
 }
