f05b16b0e6c0414734feb17d55b63c467b4611db
julien <julienvillessot@gmail.com>
2024-11-07 08:42:29 +0000

    strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,29 @@
+/**
+ * _strpbrk - check description
+ * @s: string
+ * @accept: string
+ * Description: string pointer break
+ * Return: s, 0
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
+	int v;
+	unsigned int c;
 
 	i = 0;
-
-	while(s[i] != '\0')
+	c = 0;
+	while (s[i] != '\0')
 	{
-		while(
+		v = 0;
+		while (accept[v] != '\0')
+		{
+			if (accept[v] == s[i])
+				return (s + c);
+			v++;
+		}
+		c++;
+		i++;
+	}
+	return (0);
+}
