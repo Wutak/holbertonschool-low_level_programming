1925860629ff710393401887e5c8055d4c33d388
julien <julienvillessot@gmail.com>
2024-11-07 13:57:23 +0000

    strlen

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -13,19 +13,3 @@ int _strlen_recursion(char *s)
 	else
 		return 1 + _strlen_recursion(s + 1);
 }
-
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int n;
-
-    n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
-    return (0);
-}
