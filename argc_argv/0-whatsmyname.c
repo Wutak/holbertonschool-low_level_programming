d185afae3f2c0009c97603552447ace8f1f10b0f
julien <julienvillessot@gmail.com>
2024-11-08 14:33:13 +0000

    myname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check desc
+ * @argc: argument
+ * @argv: value
+ * desc: print own name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
