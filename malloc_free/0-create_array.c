0b5c19ef7750e35d16da92049f9cc30d93376025
julien <julienvillessot@gmail.com>
2024-11-12 09:21:24 +0000

    array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+/**
+ * create_array - check desc
+ * @size: size of array
+ * @c: character
+ * desc: create array
+ * Return: 0
+ */
+char *create_array(unnsigned int size, char c)
+{
+	char *a;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (0);
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
+
+	while (i < size);
+	{
+		a[i] = c;
+		i++;
+	}
+	return (a);
+}
