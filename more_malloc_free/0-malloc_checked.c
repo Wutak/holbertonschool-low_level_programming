7332ed85f49e77746389bfde7b087f06070c70cc
julien <julienvillessot@gmail.com>
2024-11-13 09:21:27 +0000

    checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - check desc
+ * @b: number
+ * desc: checking malloc
+ * Return: 1
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (ptr);
+}
