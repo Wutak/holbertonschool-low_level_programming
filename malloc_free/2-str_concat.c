43d84327bf57cad25672a421f5f17df036abbcd9
julien <julienvillessot@gmail.com>
2024-11-14 14:09:51 +0000

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,7 +8,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1 = 0, len2 = 0;
+	int i = 0, j = 0, len1 = 0, len2 = 0;
 	char *p;
 
 	if (s1 == NULL)
@@ -25,7 +25,6 @@ char *str_concat(char *s1, char *s2)
 	p = malloc((len1 + len2 + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-
 	while (i < len1)
 	{
 		p[i] = s1[i];
