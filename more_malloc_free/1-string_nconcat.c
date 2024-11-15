57370c04a61d933be856f2fc8eab2288fdc8a104
julien <julienvillessot@gmail.com>
2024-11-15 12:21:36 +0000

    string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n < len2)
 		len2 = n;
-	v = len1 = len2 = 1;
+	v = len1 + len2 + 1;
 	p = malloc(v * sizeof(char));
 	if (p == NULL)
 		return (NULL);
