f43b63e043af533d2f0f19bbc4fb7f904ba15678
julien <julienvillessot@gmail.com>
2024-11-15 14:24:19 +0000

    init

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - dog
+ * @d: pointer
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: 1
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
