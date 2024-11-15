7528dce5866af00bfbc80a322da57fb8de713061
julien <julienvillessot@gmail.com>
2024-11-15 12:10:53 +0000

    strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,51 +2,41 @@
 #include <stdlib.h>
 
 /**
- * _strlen - check desc
+ * _strlen - strlen
  * @s: pointer
- * desc: strlen
  * Return: v
  */
 int _strlen(char *s)
 {
 	int v = 0;
 
-	while (s[v] != '\0'[v++])
-	{
-
-	}
+	while (s[v] != '\0')
+		v++;
 	return (v);
 }
 
 /**
- * *_strdup - check desc
+ * *_strdup - strdup
  * @str: pointer
- * desc: strdup
  * Return: p
  */
-
 char *_strdup(char *str)
 {
 	char *p;
-	int size;
-	int i;
+	int size, i;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
+
+
 	size = _strlen(str) + 1;
 	p = malloc(size * sizeof(char));
 
-	i = 0;
-	while (i < size)
-	{
-		if (p == NULL)
-		{
-			return (NULL);
-	}
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
 		p[i] = str[i];
-		i++;
-	}
+
 	return (p);
 }
