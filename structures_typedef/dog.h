a86a390ed1faf193b0e31bef0c250907382d97f3
julien <julienvillessot@gmail.com>
2024-11-15 13:47:35 +0000

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,15 @@
-#include <stdio.h>
-
-struct my_dog;
+#ifndef DOG_h
+#define DOG_h
+/**
+ * struct dog - dog
+ * @name : char
+ * @age: float
+ * @owner: char
+ * Return:  nothing
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t;
