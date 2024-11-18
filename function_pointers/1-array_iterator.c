bbffaa0da62c704587e0a65c69c4fe65bc1373a5
julien <julienvillessot@gmail.com>
2024-11-18 15:04:50 +0000

    array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - array
+ * @array: pointer
+ * @size_t: size
+ * @action: function
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+	while (i < size)
+	{
+		action(elem);
+		i++;
+	}
+}
