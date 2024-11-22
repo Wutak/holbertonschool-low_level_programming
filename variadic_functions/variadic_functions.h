056a661cf01bc50ce95b59d7ab93aeb2d6a78e71
julien <julienvillessot@gmail.com>
2024-11-22 13:00:14 +0000

    sum

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef "variadic_functions.h"
-#define "variadic_functions.h"
+#ifndef "VARIADIC_FUCNTIONS_H"
+#define "VARIADIC_FUNCTIONS_H"
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
