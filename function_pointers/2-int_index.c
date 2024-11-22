86e6df74044c9592da2116c938ab744c0de3fce4
julien <julienvillessot@gmail.com>
2024-11-22 09:00:31 +0000

    index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -9,13 +9,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
+
 	if (size < 1 || array == NULL || cmp == NULL)
 		return (-1);
 
 	while (i < size)
 	{
 		if (cmp(array[i]))
-			return(i);
+			return (i);
 		i++;
 	}
 	return (-1);
