18b9e5868f9d6eb7f36e1c7b447de198219b2bff
julien <julienvillessot@gmail.com>
2024-12-03 12:46:23 +0000

    printf list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,3 +1,17 @@
+#include "lists.h"
+
 size_t print_list(const list_t *h)
 {
-
+	size_t life;
+	life = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("%d %s\n", 0, "(nil)");
+		else
+			printf("%d %s\n", h->len, h-str);
+		h = h->nnext;
+		life++;
+	}
+	return(life);
+}
