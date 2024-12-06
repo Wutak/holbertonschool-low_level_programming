ba9f504f9aff42066982c99ef103584381a16786
julien <julienvillessot@gmail.com>
2024-12-06 14:08:31 +0000

    push

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,5 +1,21 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: int
+ * @next: points to next node
+ * Return: Nothing
+ */
+typedef struct dlistint_t
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 size_t print_dtlistint(const dlistint_t *h);
 
 #endif
